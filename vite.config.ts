df8a04208a69267c7624341c17eccc84bbabae0f
shivamnautiyal06 <108185267+shivamnautiyal06@users.noreply.github.com>
2025-07-02 16:31:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // In KB — raises warning threshold to 1MB
+  },
 });
