03f8abf0b3a1d45b649f3f1588fafd3787d2b2d7
shivamnautiyal06 <108185267+shivamnautiyal06@users.noreply.github.com>
2025-07-04 15:48:38 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,22 +1,26 @@
-import { next } from "@vercel/edge";
+import { next } from '@vercel/edge';
 
-export const config = { matcher: "/(.*)" };
+export const config = {
+  matcher: [
+    '/((?!.*\\.(png|jpg|jpeg|svg|ico|json|js|css|map)).*)',
+  ],
+};
 
 export default function middleware(request) {
-  const authHeader = request.headers.get("authorization");
-  if (authHeader) {
-    const base64 = authHeader.split(" ")[1];
-    const [user, pass] = atob(base64).split(":");
+  const auth = request.headers.get('authorization');
+  if (auth) {
+    const [u, p] = atob(auth.split(' ')[1]).split(':');
     if (
-      user === process.env.BASIC_AUTH_USER &&
-      pass === process.env.BASIC_AUTH_PASS
+      u === process.env.BASIC_AUTH_USER &&
+      p === process.env.BASIC_AUTH_PASS
     ) {
-      return next(); // Auth passed
+      return next();
     }
   }
-  return new Response("Unauthorized", {
+  return new Response('Unauthorized', {
     status: 401,
-    headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' },
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
   });
 }
-  
